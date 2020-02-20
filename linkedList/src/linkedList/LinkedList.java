03337a2334724ab4959ecb196e9f462a8f2ca331
TinaHodepina <31168143+TinaRam@users.noreply.github.com>
2020-02-20 21:12:28 +0100

    Testklasse ferdig

--- linkedList/src/linkedList/LinkedList.java
+++ linkedList/src/linkedList/LinkedList.java
@@ -9,50 +9,46 @@ public class LinkedList {
 	public Node head;
 
 	public void addToFront(int data) {
-		Node newNode = new Node(); // Henter node fra systemet. Data og next = NULL
-		newNode.data = data; // Fyller den med data
+		Node newNode = new Node(data); // Henter node fra systemet. Fyller den med data. next = NULL
 		newNode.next = head; // Justerer peker til å peke på den første noden i listen
 		head = newNode; // Deklarerer ny node som head
 
-		System.out.println(data + " ble lagt først i lista.");
-		printList();
+		//		System.out.println("[ " + data + " ] " + "er lagt først i lista.");
 	}
 
 	// 1: Slett element først i listen
 	public void removeFromFront() {
 		if (head == null) {
-			System.out.println("Lista er tom...\n");
+			System.out.println("Lista er tom...");
 		} else {
 			int deletedNode = head.data;
 			head = head.next;
-			System.out.println("Sletting vellykket! Node [ " + deletedNode + " ] er nå slettet.\n");
-
-			printList();
+			System.out.println("Sletting vellykket! Node [ " + deletedNode + " ] er nå slettet.");
 		}
 	}
 
 	// 2: Legg til element i slutten av listen
 	public void addToBack(int value) {
+		Node newNode = new Node(value); // Henter node fra systemet og Fyller den med data (peker = null)
+
 		if (head == null) {
-			addToFront(value);
+			head = newNode;
 		} else {
 			Node last = head; // Lager en node som peker på samme som head
 			while (last.next != null) { // om ikke last er siste elementet i lista
 				last = last.next; // flytter last til neste node
 			}
-			Node newNode = new Node(); // Henter node fra systemet
-			newNode.data = value; // Fyller den med data (peker = null)
+
 			last.next = newNode; // Justerer siste elements peker til å peke på ny node
 
-			System.out.println(value + " lagt til slutten av lista.");
-			printList();
+			//			System.out.println("[ " + value + " ] " + "er lagt til slutten av lista.");
 		}
 	}
 
 	// 3: Slett et element i slutten av listen
 	public void removeFromBack() {
 		if (head == null) {
-			System.out.println("Lista er tom...\n");
+			System.out.println("Lista er tom...");
 		} else {
 			Node current = head;
 			Node prev = head;
@@ -62,25 +58,24 @@ public class LinkedList {
 			}
 			int deletedNode = current.data;
 			if (current == head) {
-				head = head.next; // head.next vil være null på dette tidspunktet
+				head = null;
 			} else {
-				prev.next = current.next; // current.next vil være null på dette tidspunktet
+				prev.next = null;
 			}
-			System.out.println("Sletting vellykket! Node [ " + deletedNode + " ] er nå slettet.\n");
-			printList();
+			System.out.println("Sletting vellykket! Node [ " + deletedNode + " ] er nå slettet.");
 		}
 	}
 
 	// 4: Slett et element med oppgitt verdi (sletter kun den første verdien den finner)
 	public void removeNodeWithValue(int value) {
 		if (head == null) {
-			System.out.println("Lista er tom...\n");
+			System.out.println("Lista er tom...");
 		} else {
 			Node current = head;
 			Node prev = head;
 
 			if (current.data == value && current == head) { // Om noden som skal slettes er første noden i listen
-				head = current.next; // Sletter første node
+				head = head.next; // Sletter første node
 			}
 
 			while (current != null && current.data != value) { // Om current ikke er tom eller har verdi som skal slettes...
@@ -94,14 +89,13 @@ public class LinkedList {
 			} else {
 				System.out.println("Ingen node med verdien '" + value + "' i lista!");
 			}
-			printList();
 		}
 	}
 
 	// 5: Legg til et element etter et element med oppgitt verdi
 	public void addAfterValue(int valueExistingNode, int valueNewNode) {
 		if (head == null) {
-			System.out.println("Lista er tom...\n");
+			System.out.println("Lista er tom...");
 		} else {
 
 			Node current = head;
@@ -110,21 +104,19 @@ public class LinkedList {
 			}
 
 			if (current != null) { // verdi funnet
-				Node newNode = new Node();
-				newNode.data = valueNewNode;
+				Node newNode = new Node(valueNewNode);
 				newNode.next = current.next; // Juster peker ny node til å peke på current sin neste
 				current.next = newNode; // Juster peker current til å peke på ny node
 			} else {
-				System.out.println("Lista har ingen elementer med verdien '" + valueExistingNode + "'!\n");
+				System.out.println("Lista har ingen elementer med verdien '" + valueExistingNode + "'!");
 			}
-			printList();
 		}
 	}
 
 	// 6: Legg til et element foran et element med oppgitt verdi
 	public void addBeforeValue(int valueExistingNode, int valueNewNode) {
 		if (head == null) {
-			System.out.println("Lista er tom...\n");
+			System.out.println("Lista er tom...");
 		} else {
 
 			if (head.data == valueExistingNode) {
@@ -139,15 +131,13 @@ public class LinkedList {
 				}
 
 				if (current != null) {
-					Node newNode = new Node();
-					newNode.data = valueNewNode;
+					Node newNode = new Node(valueNewNode);
 					newNode.next = current;
 					prev.next = newNode;
-					System.out.println(valueNewNode + " ble lagt listen før node " + valueExistingNode + ".\n");
+					System.out.println(valueNewNode + " ble lagt i listen før node " + valueExistingNode + ".");
 				} else {
-					System.out.println("Det finnes ingen elementer med verdien '" + valueExistingNode + "' i lista.\n");
+					System.out.println("Det finnes ingen elementer med verdien '" + valueExistingNode + "' i lista.");
 				}
-				printList();
 			}
 		}
 	}
@@ -161,9 +151,9 @@ public class LinkedList {
 			length++;
 		}
 		if (length == 1) {
-			System.out.println("Det er " + length + " element i lista.\n");
+			System.out.println("Det er " + length + " element i lista.");
 		} else {
-			System.out.println("Det er " + length + " elementer i lista.\n");
+			System.out.println("Det er " + length + " elementer i lista.");
 		}
 	}
 
@@ -180,16 +170,16 @@ public class LinkedList {
 		}
 
 		if (instances == 1) {
-			System.out.println("Det er " + instances + " element med verdien '" + value + "'.\n");
+			System.out.println("Det er " + instances + " element med verdien '" + value + "'.");
 		} else {
-			System.out.println("Listen har " + instances + " elementer med verdien '" + value + "'.\n");
+			System.out.println("Listen har " + instances + " elementer med verdien '" + value + "'.");
 		}
 	}
 
 	// 9: Skriv ut hele listen
 	public void printList() {
 		if (head == null) {
-			System.out.println("Lista er tom...\n");
+			System.out.println("Lista er tom...");
 		} else {
 			System.out.print("head -->");
 			Node current = head;
@@ -211,7 +201,32 @@ public class LinkedList {
 			head = head.next;
 			deletedNodes++;
 		}
-		System.out.println("Liste med " + deletedNodes + " elementer slettet.\n");
+		if (deletedNodes == 0) {
+			System.out.println("Lista er allerede tom...Ingen elementer slettet.");
+		} else {
+			System.out.println("Liste med " + deletedNodes + " elementer slettet.");
+		}
+	}
+
+	// kun for testing
+	public String getListAsString() {
+		String s;
+
+		if (head == null) {
+			s = "Lista er tom...";
+		} else {
+			s = "head -->";
+			Node current = head;
+			while (current != null) {
+				s += " [ " + current.data + " ] ";
+				if (current.next != null) {
+					s += "-->";
+				}
+				current = current.next;
+			}
+			s += "~~";
+		}
+		return s;
 	}
 
 }
--- linkedList/src/linkedList/TestLinkedList.java
+++ linkedList/src/linkedList/TestLinkedList.java
@@ -17,53 +17,306 @@ public class TestLinkedList {
 		// # ENDRE TESTKLASSE TIL Å KJØRE ALLE PLANLAGTE TESTER MED UTSKRIFT FOR HVER TEST!
 
 		// 1: Slett element først i listen
-		LinkedList list1 = new LinkedList();
-		System.out.println("1: Slett element først i listen");
-		System.out.println();
+		LinkedList list = new LinkedList();
+		System.out.println("****** START TEST 1 - Slett element først i listen: ******\n");
 
 		// Tom liste
-		System.out.print("Slett første element fra tom liste --> ");
-		list1.removeFromFront();
+		System.out.println("Slett første element fra tom liste. ( head ~~ )");
+		list.removeFromFront();
 		System.out.println();
 
 		// Liste med 1 element
-		list1.addToFront(10);
-		System.out.print("Liste med 1 element: ");
-		list1.removeFromFront();
+		list.addToFront(1);
+		System.out.print("Slett første element fra liste med ett element. ( " + list.getListAsString() + " )\n");
+		list.removeFromFront();
 		System.out.println();
 
 		// Liste med flere elementer
-		list1.addToBack(1);
-		list1.addToBack(2);
-		list1.addToBack(3);
-		list1.addToBack(4);
-		System.out.print("Liste med fire elementer: ");
-		list1.removeFromFront();
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		System.out.print("Slett første element fra liste med flere elementer. ( " + list.getListAsString() + " )\n");
+		list.removeFromFront();
+		System.out.println("\n****** SLUTT TEST 1 ****** \n");
+
+		list.head = null;
 		System.out.println();
 
 		// 2: Legg til element i slutten av listen
+		System.out.println("****** START TEST 2 - Legg til element i slutten av listen: ******\n");
+
+		// Tom liste
+		System.out.println("Legg til '1' i slutten når liste er tom. ( head ~~ )");
+		list.addToBack(1);
+		list.printList();
+
+		// Liste med 1 element
+		System.out.print("Legg til '2' i slutten når liste har ett element. ( " + list.getListAsString() + " )\n");
+		list.addToBack(2);
+		list.printList();
+
+		// Liste med flere elementer
+		System.out.print("Legg til '3' i slutten når liste har flere elementer. ( " + list.getListAsString() + " )\n");
+		list.addToBack(3);
+		list.printList();
+		System.out.println("\n****** SLUTT TEST 2 ****** \n");
+
+		list.head = null;
+		System.out.println();
 
 		// 3: Slett et element i slutten av listen
+		System.out.println("****** START TEST 3 - Slett et element i slutten av listen: ******\n");
+
+		// Tom liste
+		System.out.println("Slett siste element fra tom liste. ( head ~~ )");
+		list.removeFromBack();
+		System.out.println();
+
+		// Liste med 1 element
+		list.addToBack(1);
+		System.out.print("Slett siste element fra liste med ett element. ( " + list.getListAsString() + " )\n");
+		list.removeFromBack();
+		System.out.println();
+
+		// Liste med flere elementer
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		System.out.print("Slett siste element fra liste med flere elementer. ( " + list.getListAsString() + " )\n");
+		list.removeFromBack();
+		System.out.println("\n****** SLUTT TEST 3 ****** \n");
+
+		list.head = null;
+		System.out.println();
 
 		// 4: Slett et element med oppgitt verdi
+		System.out.println("****** START TEST 4 - Slett et element med oppgitt verdi: ******\n");
+
+		// Tom liste
+		System.out.println("Slett element '0' når liste er tom. ( head ~~ )");
+		list.removeNodeWithValue(0);
+		System.out.println();
+
+		// Liste uten oppgitt verdi
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		System.out.print("Slett element '0' når liste ikke har element '0'. ( " + list.getListAsString() + " )\n");
+		list.removeNodeWithValue(0);
+		System.out.println();
+
+		// Oppgitt verdi i head
+		list.addToFront(0);
+		System.out.print("Slett element '0' når '0' er første node ( " + list.getListAsString() + " )\n");
+		list.removeNodeWithValue(0);
+		System.out.println();
+
+		// Liste har flere noder med oppgitt verdi
+		list.addAfterValue(1, 0);
+		list.addToBack(0);
+		System.out.print("Slett element '0' når liste har flere noder med '0' ( " + list.getListAsString() + " )\n");
+		list.removeNodeWithValue(0);
+		list.printList();
+
+		// Oppgitt verdi siste node
+		System.out.print("Slett element '0' når '0' er siste node ( " + list.getListAsString() + " )\n");
+		list.removeNodeWithValue(0);
+		list.printList();
+
+		System.out.println("****** SLUTT TEST 4 ****** \n");
+
+		list.head = null;
+		System.out.println();
 
 		// 5: Legg til et element etter et element med oppgitt verdi
+		System.out.println("****** START TEST 5 - Legg til et element etter et element med oppgitt verdi: ******\n");
+
+		// Tom liste
+		System.out.println("Legg til '9' etter '0' når liste er tom. ( head ~~ )");
+		list.addAfterValue(0, 9);
+		System.out.println();
+
+		// Liste uten oppgitt verdi
+		list.addToBack(1);
+		list.addToBack(2);
+		System.out.print("Legg til '9' etter '0' når liste ikke har '0'. ( " + list.getListAsString() + " )\n");
+		list.addAfterValue(0, 9);
+		System.out.println();
+
+		// Oppgitt verdi i head
+		list.addToFront(0);
+		System.out.print("Legg til '9' etter '0' når '0' er første node ( " + list.getListAsString() + " )\n");
+		list.addAfterValue(0, 9);
+		list.printList();
+
+		// Liste har flere noder med oppgitt verdi
+		list.head = null;
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		list.addAfterValue(1, 0);
+		list.addAfterValue(2, 0);
+		System.out.print("Legg til '9' etter '0' når liste har flere noder '0' ( " + list.getListAsString() + " )\n");
+		list.addAfterValue(0, 9);
+		list.printList();
+
+		// Oppgitt verdi siste node
+		list.head = null;
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(0);
+		System.out.print("Legg til '9' etter '0' når '0' er siste node ( " + list.getListAsString() + " )\n");
+		list.addAfterValue(0, 9);
+		list.printList();
+
+		System.out.println("****** SLUTT TEST 5 ****** \n");
+
+		list.head = null;
+		System.out.println();
 
 		// 6: Legg til et element foran et element med oppgitt verdi
+		System.out.println("****** START TEST 6 - Legg til et element foran et element med oppgitt verdi: ******\n");
+
+		// Tom liste
+		System.out.println("Legg til '9' foran '0' når liste er tom. ( head ~~ )");
+		list.addBeforeValue(0, 9);
+		System.out.println();
+
+		// Liste uten oppgitt verdi
+		list.addToBack(1);
+		list.addToBack(2);
+		System.out.print("Legg til '9' foran '0' når liste ikke har '0'. ( " + list.getListAsString() + " )\n");
+		list.addBeforeValue(0, 9);
+		System.out.println();
+
+		// Oppgitt verdi i head
+		list.addToFront(0);
+		System.out.print("Legg til '9' foran '0' når '0' er første node ( " + list.getListAsString() + " )\n");
+		list.addBeforeValue(0, 9);
+		list.printList();
+
+		// Liste har flere noder med oppgitt verdi
+		list.head = null;
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		list.addAfterValue(1, 0);
+		list.addAfterValue(2, 0);
+		System.out.print("Legg til '9' foran '0' når liste har flere noder '0' ( " + list.getListAsString() + " )\n");
+		list.addBeforeValue(0, 9);
+		list.printList();
+
+		// Oppgitt verdi siste node
+		list.head = null;
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(0);
+		System.out.print("Legg til '9' foran '0' når '0' er siste node ( " + list.getListAsString() + " )\n");
+		list.addBeforeValue(0, 9);
+		list.printList();
+
+		System.out.println("****** SLUTT TEST 6 ****** \n");
+
+		list.head = null;
+		System.out.println();
 
 		// 7: Skriv ut lengden på listen
+		System.out.println("****** START TEST 7 - Skriv ut lengden på listen: ******\n");
+
+		// Liste er tom
+		System.out.println("Skriv ut lengden på listen når den er tom. ( head ~~ )");
+		list.printListLength();
+		System.out.println();
+
+		// Liste ikke er tom
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		System.out.print("Skriv ut lengden på listen når den ikke er tom.  ( " + list.getListAsString() + " )\n");
+		list.printListLength();
+
+		System.out.println("\n****** SLUTT TEST 7 ****** \n");
+
+		list.head = null;
+		System.out.println();
 
 		// 8: Skriv ut antall forekomster av elementer med oppgitt verdi
 
+		System.out
+				.println("****** START TEST 8 - Skriv ut antall forekomster av elementer med oppgitt verdi: ******\n");
+
+		// Liste er tom
+		System.out.println("Antall forekomster av '0' når lista er tom. ( head ~~ )");
+		list.printInstancesOfValue(0);
+		System.out.println();
+
+		// Liste har ikke oppgitt verdi
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		System.out.print("Antall forekomster av '0' når liste ikke har '0'. ( " + list.getListAsString() + " )\n");
+		list.printInstancesOfValue(0);
+		System.out.println();
+
+		// Liste har oppgitt verdi
+		list.head = null;
+		list.addToBack(0);
+		list.addToBack(1);
+		list.addToBack(0);
+		list.addToBack(0);
+		list.addToBack(2);
+		list.addToBack(3);
+		list.addToBack(0);
+		System.out.print("Antall forekomster av '0' når liste ikke har '0'. ( " + list.getListAsString() + " )\n");
+		list.printInstancesOfValue(0);
+
+		System.out.println("\n****** SLUTT TEST 8 ****** \n");
+
+		list.head = null;
+		System.out.println();
+
 		// 9: Skriv ut hele listen
+		System.out.println("****** START TEST 9 - Skriv ut hele listen: ******\n");
+
+		// Liste er tom
+		System.out.println("Skriv ut hele listen når den er tom. ( head ~~ )");
+		list.printList();
+		System.out.println();
+
+		// Liste ikke er tom
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		System.out.print("Skriv ut hele listen når den ikke er tom.  ( " + list.getListAsString() + " )\n");
+		list.printList();
+
+		System.out.println("****** SLUTT TEST 9 ****** \n");
+
+		list.head = null;
+		System.out.println();
 
 		// 10: Slett listen og vis antall slettede elementer
+		System.out.println("****** START TEST 10 - Slett listen og vis antall slettede elementer: ******\n");
 
-		test(list1.head == null, true, "");
+		// Liste er tom
+		System.out.println("Slett listen når liste er tom. ( head ~~ )");
+		list.deleteList();
+		System.out.println();
+
+		// Liste ikke er tom
+		list.addToBack(1);
+		list.addToBack(2);
+		list.addToBack(3);
+		System.out.print("Slett listen når liste ikke er tom.  ( " + list.getListAsString() + " )\n");
+		list.deleteList();
+
+		System.out.println("\n****** SLUTT TEST 10 ****** \n");
+
+		//		test(list1.head == null, true, "test-metode");
 	}
 
 	/**
-	 * @param test -- Metode som testest
+	 * @param test -- Metode som testes
 	 * @param expected -- Forventet resultat?
 	 * @param message -- Print melding som forklarer hva som er testet
 	 */
